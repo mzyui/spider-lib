0f032acb5d1405559161417c77d0bc5c5e0f6e05
mzyui <xnver404@gmail.com>
2026-03-06 01:36:00 +0700

    docs(util): document metrics and http client contracts

--- spider-downloader/src/traits.rs
+++ spider-downloader/src/traits.rs
@@ -10,10 +10,15 @@ use spider_util::error::SpiderError;
 /// A trait for HTTP downloaders that can fetch web pages and apply middleware
 #[async_trait]
 pub trait Downloader: Send + Sync + 'static {
+    /// Concrete HTTP client type used by the downloader.
     type Client: Send + Sync;
 
     /// Download a web page using the provided request.
     /// This function focuses solely on executing the HTTP request.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error when request execution fails.
     async fn download(&self, request: Request) -> Result<Response, SpiderError>;
 
     /// Returns a reference to the underlying HTTP client.
